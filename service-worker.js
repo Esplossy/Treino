ace74dedd734f0ee6ce2d97be446de4ac5023432
Esplossy <digoohclash@gmail.com>
2026-02-22 15:52:21 -0300

    Implement service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "vshape-cache-v1";
+const urlsToCache = [
+  "/Treino/app-treino-4.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
